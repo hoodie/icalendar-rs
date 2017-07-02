b4594d29bff013913c1133a3b36ee363bfd1e46c
Hendrik Sollich <hendrik@hoodie.de>
2017-07-02 22:47:02 +0200

    fixed examples

--- examples/write.rs
+++ examples/write.rs
@@ -7,11 +7,11 @@ fn main() {
     let event = Event::new()
         .summary("test event")
         .description("here I have something really important to do")
-        .starts(UTC::now())
+        .starts(Utc::now())
         .class(Class::Confidential)
         //.repeats_every(15.days())
         //.repeats(Every::second().wednesday())
-        .ends(UTC::now() + Duration::days(1))
+        .ends(Utc::now() + Duration::days(1))
         //.all_day()
         .append_property(Property::new("TEST", "FOOBAR")
                   .add_parameter("IMPORTANCE", "very")
@@ -20,8 +20,8 @@ fn main() {
         .done();
 
     let bday = Event::new()
-        .start_date(UTC.ymd(2016, 3, 15))
-        .end_date(UTC.ymd(2016, 3, 15))
+        .start_date(Utc.ymd(2016, 3, 15))
+        .end_date(Utc.ymd(2016, 3, 15))
         .summary("My Birthday")
         .description(
 r#"Hey, I'm gonna have a party
@@ -31,7 +31,7 @@ Hendrik"#
         .done();
 
     let bday2 = Event::new()
-        .all_day(UTC.ymd(2016, 3, 15))
+        .all_day(Utc.ymd(2016, 3, 15))
         .done();
 
     let todo = Todo::new().summary("Buy some milk").done();
--- src/lib.rs
+++ src/lib.rs
@@ -19,9 +19,9 @@
 //! let event = Event::new()
 //!     .summary("test event")
 //!     .description("here I have something really important to do")
-//!     .starts(UTC::now())
+//!     .starts(Utc::now())
 //!     .class(Class::Confidential)
-//!     .ends(UTC::now() + Duration::days(1))
+//!     .ends(Utc::now() + Duration::days(1))
 //!     .append_property(Property::new("TEST", "FOOBAR")
 //!               .add_parameter("IMPORTANCE", "very")
 //!               .add_parameter("DUE", "tomorrow")
@@ -29,7 +29,7 @@
 //!     .done();
 //!
 //! let bday = Event::new()
-//!     .all_day(UTC.ymd(2016, 3, 15))
+//!     .all_day(Utc.ymd(2016, 3, 15))
 //!     .summary("My Birthday")
 //!     .description(
 //! r#"Hey, I'm gonna have a party
