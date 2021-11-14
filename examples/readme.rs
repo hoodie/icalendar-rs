4664ba6fb8dbcf38a35e2d63172a440c8ab9d43f
Hendrik Sollich <hendrik@hoodie.de>
2021-11-14 22:31:10 +0100

    docs: overhaul README

--- examples/readme.rs
+++ examples/readme.rs
@@ -2,34 +2,36 @@ use chrono::*;
 use icalendar::*;
 
 fn main() {
-    let event = Event::new()
-        .summary("test event")
-        .description("here I have something really important to do")
-        .starts(Utc::now())
-        .class(Class::Confidential)
-        .ends(Utc::now() + Duration::days(1))
-        .append_property(
-            Property::new("TEST", "FOOBAR")
-                .add_parameter("IMPORTANCE", "very")
-                .add_parameter("DUE", "tomorrow")
+    let my_calendar = Calendar::new()
+        .push(
+            Event::new()
+                .summary("test event")
+                .description("here I have something really important to do")
+                .starts(Utc::now())
+                .class(Class::Confidential)
+                .ends(Utc::now() + Duration::days(1))
+                .append_property(
+                    Property::new("TEST", "FOOBAR")
+                        .add_parameter("IMPORTANCE", "very")
+                        .add_parameter("DUE", "tomorrow")
+                        .done(),
+                )
+                .done(),
+        )
+        .push(
+            Todo::new()
+                .summary("groceries")
+                .description("Buy some milk")
+                .done(),
+        )
+        .push(
+            Event::new()
+                .all_day(Utc.ymd(2016, 3, 15))
+                .summary("My Birthday")
+                .description("Hey, I'm gonna have a party\nBYOB: Bring your own beer.\nHendrik")
                 .done(),
         )
         .done();
 
-    let bday = Event::new()
-        .all_day(Utc.ymd(2016, 3, 15))
-        .summary("My Birthday")
-        .description(
-            r#"Hey, I'm gonna have a party
-    BYOB: Bring your own beer.
-    Hendrik"#,
-        )
-        .done();
-
-    let todo = Todo::new().summary("Buy some milk").done();
-
-    let mut calendar = Calendar::new();
-    calendar.push(event);
-    calendar.push(todo);
-    calendar.push(bday);
+    println!("{}", my_calendar)
 }
