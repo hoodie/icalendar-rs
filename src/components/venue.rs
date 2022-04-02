174947a495699e4e3146a2f7034b64df0f7bddc5
Andrew Walbran <qwandor@google.com>
2022-04-02 21:39:16 +0100

    feat: Add getters for venue properties.

--- src/components/venue.rs
+++ src/components/venue.rs
@@ -27,6 +27,11 @@ impl Venue {
         self.add_property("STREET-ADDRESS", address)
     }
 
+    /// Gets the value of the STREET-ADDRESS `Property`.
+    pub fn get_street_address(&self) -> Option<&str> {
+        self.property_value("STREET-ADDRESS")
+    }
+
     /// Set the EXTENDED-ADDRESS `Property`
     ///
     /// This property provides the opportunity to include extended address information for a
@@ -37,11 +42,21 @@ impl Venue {
         self.add_property("EXTENDED-ADDRESS", address)
     }
 
+    /// Gets the value of the EXTENDED-ADDRESS `Property`.
+    pub fn get_extended_address(&self) -> Option<&str> {
+        self.property_value("EXTENDED-ADDRESS")
+    }
+
     /// Set the LOCALITY `Property`
     ///
     /// This specifies the city or locality of a venue.
     pub fn locality(&mut self, locality: &str) -> &mut Self {
         self.add_property("LOCALITY", locality)
+    }
+
+    /// Gets the value of the LOCALITY `Property`.
+    pub fn get_locality(&self) -> Option<&str> {
+        self.property_value("LOCALITY")
     }
 
     /// Set the REGION `Property`
@@ -51,6 +66,11 @@ impl Venue {
         self.add_property("REGION", region)
     }
 
+    /// Gets the value of the REGION `Property`.
+    pub fn get_region(&self) -> Option<&str> {
+        self.property_value("REGION")
+    }
+
     /// Set the COUNTRY `Property`
     ///
     /// This specifies the country of a location.
@@ -58,10 +78,54 @@ impl Venue {
         self.add_property("COUNTRY", country)
     }
 
+    /// Gets the value of the COUNTRY `Property`.
+    pub fn get_country(&self) -> Option<&str> {
+        self.property_value("COUNTRY")
+    }
+
     /// Set the POSTAL-CODE `Property`
     ///
     /// This specifies the postal code of a location.
     pub fn postal_code(&mut self, postal_code: &str) -> &mut Self {
         self.add_property("POSTAL-CODE", postal_code)
+    }
+
+    /// Gets the value of the POSTAL-CODE `Property`.
+    pub fn get_postal_code(&self) -> Option<&str> {
+        self.property_value("POSTAL-CODE")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_properties_unset() {
+        let venue = Venue::new();
+        assert_eq!(venue.get_street_address(), None);
+        assert_eq!(venue.get_extended_address(), None);
+        assert_eq!(venue.get_locality(), None);
+        assert_eq!(venue.get_region(), None);
+        assert_eq!(venue.get_country(), None);
+        assert_eq!(venue.get_postal_code(), None);
+    }
+
+    #[test]
+    fn get_properties_set() {
+        let venue = Venue::new()
+            .street_address("street address")
+            .extended_address("extended address")
+            .locality("locality")
+            .region("region")
+            .country("country")
+            .postal_code("postal code")
+            .done();
+        assert_eq!(venue.get_street_address(), Some("street address"));
+        assert_eq!(venue.get_extended_address(), Some("extended address"));
+        assert_eq!(venue.get_locality(), Some("locality"));
+        assert_eq!(venue.get_region(), Some("region"));
+        assert_eq!(venue.get_country(), Some("country"));
+        assert_eq!(venue.get_postal_code(), Some("postal code"));
     }
 }
