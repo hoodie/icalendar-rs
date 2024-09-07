70d442a00f0a6db348bf5966f356ae90b76ca278
Praxis | Darian <hello@vulpinecat.com>
2024-09-07 10:18:05 +0200

    fix: remove colon escape in text property

--- src/properties.rs
+++ src/properties.rs
@@ -153,7 +153,6 @@ impl Property {
             .replace('\\', r#"\\"#)
             .replace(',', r#"\,"#)
             .replace(';', r#"\;"#)
-            .replace(':', r#"\:"#)
             .replace('\n', r#"\N"#)
     }
 
