d40ec2f1fc7e41db9fbd3ec34cba6a33cc8e4812
Thomas den Hollander <denhollander.thomas@gmail.com>
2024-10-24 11:59:39 +0200

    docs: Use readme as top level documentation

--- tests/calendar.rs
+++ tests/calendar.rs
@@ -62,3 +62,39 @@ fn test_calendar_to_string() {
     calendar.push(todo);
     assert_eq!(calendar.to_string(), EXPECTED_CAL_CONTENT);
 }
+
+#[test]
+fn test_build_calendar() {
+    use chrono::*;
+    use icalendar::*;
+    let event = Event::new()
+        .summary("test event")
+        .description("here I have something really important to do")
+        .starts(Utc::now())
+        .class(Class::Confidential)
+        .ends(Utc::now() + Duration::days(1))
+        .append_property(
+            Property::new("TEST", "FOOBAR")
+                .add_parameter("IMPORTANCE", "very")
+                .add_parameter("DUE", "tomorrow")
+                .done(),
+        )
+        .done();
+
+    let bday = Event::new()
+        .all_day(NaiveDate::from_ymd_opt(2023, 3, 15).unwrap())
+        .summary("My Birthday")
+        .description(
+            r#"Hey, I'm gonna have a party
+BYOB: Bring your own beer.
+Hendrik"#,
+        )
+        .done();
+
+    let todo = Todo::new().summary("Buy some milk").done();
+
+    let mut calendar = Calendar::new();
+    calendar.push(event);
+    calendar.push(todo);
+    calendar.push(bday);
+}
